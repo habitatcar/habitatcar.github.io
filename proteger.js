40ea8009d7d57677010dc80467b1bb584882a6b6
habitatcar <habitatcar@gmail.com>
2026-03-31 20:57:24 -0300

    Update proteger.js

--- proteger.js
+++ proteger.js
@@ -1,13 +1,1 @@
-// proteger.js - VERSIÓN SEGURA
-import { verificarAccesoAdmin } from './admin_auth.js';
 
-// Ejecutar y ESPERAR antes de continuar
-(async () => {
-    const accesoPermitido = await verificarAccesoAdmin();
-    if (!accesoPermitido) {
-        // Detener carga de la página
-        document.body.innerHTML = '';
-        return;
-    }
-    // Si llega acá, puede continuar cargando el resto
-})();
