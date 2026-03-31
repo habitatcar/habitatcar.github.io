a8d543c2d6bb24a5d9015790549652cd356cf01a
habitatcar <habitatcar@gmail.com>
2026-03-31 18:33:54 -0300

    Update admin_auth.js

--- admin_auth.js
+++ admin_auth.js
@@ -1,4 +1,3 @@
-// admin_auth.js
 import { auth, db } from './firebase-config.js';
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
