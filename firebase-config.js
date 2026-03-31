287c3d83478c9433c45c65b47eaf8b8c2aa85cd0
habitatcar <habitatcar@gmail.com>
2026-03-31 18:34:25 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD5M0kPsdU8Hk_YX3uEahfoQ5k9PD8SUXM",
+    authDomain: "habitatcar-pro.firebaseapp.com",
+    projectId: "habitatcar-pro",
+    storageBucket: "habitatcar-pro.firebasestorage.app",
+    messagingSenderId: "311691258694",
+    appId: "1:311691258694:web:17a09051dcea7707af9347"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
